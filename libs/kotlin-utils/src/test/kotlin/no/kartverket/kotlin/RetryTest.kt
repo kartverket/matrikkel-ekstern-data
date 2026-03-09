7ecf521be40d2b51e4a7a80f328a53e23145d7c7
Nicklas Utgaard <nutgaard@gmail.com>
2026-03-09 09:26:34 +0100

    TH-2553: legger til retry-bypass mulighet

---
+++ libs/kotlin-utils/src/test/kotlin/no/kartverket/kotlin/RetryTest.kt
@@ -0,0 +1,63 @@
+package no.kartverket.kotlin
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class RetryTest {
+    @Test
+    fun `should return at once if success`() {
+        val fn = mockk<() -> String>()
+        every { fn.invoke() } returns "This is ok"
+
+        val result = retry(attempts = 3, fn = fn)
+
+        assertThat(result).isEqualTo("This is ok")
+        verify(exactly = 1) { fn.invoke() }
+    }
+
+    @Test
+    fun `should retry function N times`() {
+        val fn = mockk<() -> String>()
+        every { fn.invoke() } throws IllegalArgumentException("Not gikk feil")
+
+        assertThrows<IllegalArgumentException> {
+            retry(attempts = 3, fn = fn)
+        }
+
+        verify(exactly = 3) { fn.invoke() }
+    }
+
+    @Test
+    fun `should bypass retry on known exceptions`() {
+        val fn = mockk<() -> String>()
+        every { fn.invoke() } throws IllegalArgumentException("Not gikk feil")
+
+        assertThrows<IllegalArgumentException> {
+            retry(
+                attempts = 3,
+                stopRetryIf = { it is IllegalArgumentException },
+                fn = fn
+            )
+        }
+
+        verify(exactly = 1) { fn.invoke() }
+    }
+
+    @Test
+    fun `should retry until success`() {
+        val fn = mockk<() -> String>()
+        every { fn.invoke() } throws IllegalArgumentException("fail 1") andThenThrows
+                IllegalArgumentException("fail 2") andThen
+                "ok"
+
+        val result = retry(attempts = 3, fn = fn)
+
+        assertThat(result).isEqualTo("ok")
+        verify(exactly = 3) { fn.invoke() }
+    }
+}
